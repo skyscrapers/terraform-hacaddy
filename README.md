64bebe50fcf9fe4d1bf5f0b62cebd75dacfec404
Luca Venturelli <luca@skyscrapers.eu>
2019-07-03 04:55:25 -0500

    move module to tf 0.12 (#4)
    * move module to tf 0.12
    
    * fix readme
    
    * fix example
    
    * fix minor issues

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This module is tailered very specific for Skyscrapers environments
   * [`proxyname`]: String(optional, default "proxy"): Name to use for the proxy setup.
   * [`vpc_id`]: String(required): VPC ID where the proxies will be deployed.
   * [`subnet_count`]: Number(required): Number of subnets (to deploy de instances and for EFS targets).
-  * [`subnet_ids`]: List(required): Subnet IDs where the proxies will be deployed. Also use as EFS targets.
+  * [`subnet_ids`]: List(string)(required): Subnet IDs where the proxies will be deployed. Also use as EFS targets.
   * [`sg_all_id`]: String(required): ID of the base security group.
   * [`ami`]: String(required): AMI to be used for the proxy instances.
   * [`key_name`]: String(required): ID of the SSH key to use for the proxy instances.
@@ -19,23 +19,23 @@ This module is tailered very specific for Skyscrapers environments
 
 ## Output
 
-  * [`efs_dns_name`]: List: DNS name of the EFS volume per target (should be all the same).
-  * [`eips`]: List: Elastic IPs attached to the proxy instances. At setup you choose 1 to be master and 1 slave.
-  * [`instance_ids`]: List: IDs of the proxy instances.
+  * [`efs_dns_name`]: List(string): DNS name of the EFS volume per target (should be all the same).
+  * [`eips`]: List(string): Elastic IPs attached to the proxy instances. At setup you choose 1 to be master and 1 slave.
+  * [`instance_ids`]: List(string): IDs of the proxy instances.
   * [`sg_id`]: String: ID of the proxy instances security group.
 
 ## Example
 
 ```terraform
 module "hacaddy" {
-  source        = "github.com/skyscrapers/terraform-hacaddy?ref=cd0df2c29cdcfbfc031258df13685c8e69244c73"
-  environment   = "${var.environment}"
-  project       = "${var.project}"
+  source        = "github.com/skyscrapers/terraform-hacaddy"
+  environment   = var.environment
+  project       = var.project
   proxyname     = "proxy"
-  vpc_id        = "${module.vpc.vpc_id}"
+  vpc_id        = module.vpc.vpc_id
   subnet_count  = 3
-  subnet_ids    = ["${module.vpc.public_subnets}"]
-  sg_all_id     = "${module.securitygroups.sg_all_id}"
+  subnet_ids    = module.vpc.public_subnets
+  sg_all_id     = module.securitygroups.sg_all_id
   ami           = "ami-971238f1"
   key_name      = "philip"
   instance_type = "t3.small"
--- main.tf
+++ main.tf
@@ -1,57 +1,57 @@
 module "hacaddy_instance" {
-  source         = "github.com/skyscrapers/terraform-instances//instance?ref=2.3.6"
-  project        = "${var.project}"
-  environment    = "${var.environment}"
-  name           = "${var.proxyname}"
-  subnets        = "${var.subnet_ids}"
-  ami            = "${var.ami}"
-  key_name       = "${var.key_name}"
-  instance_type  = "${var.instance_type}"
+  source         = "github.com/skyscrapers/terraform-instances//instance?ref=3.0.1"
+  project        = var.project
+  environment    = var.environment
+  name           = var.proxyname
+  subnets        = var.subnet_ids
+  ami            = var.ami
+  key_name       = var.key_name
+  instance_type  = var.instance_type
   instance_count = 2
-  sgs            = ["${var.sg_all_id}", "${aws_security_group.sg_hacaddy.id}"]
+  sgs            = [var.sg_all_id, aws_security_group.sg_hacaddy.id]
   public_ip      = true
-  user_data      = ["${module.userdata.user_datas[0]}", "${module.userdata.user_datas[1]}"]
-  cpu_credits    = "${var.cpu_credits}"
+  user_data      = [module.userdata.user_datas[0], module.userdata.user_datas[1]]
+  cpu_credits    = var.cpu_credits
 }
 
 module "userdata" {
-  source              = "github.com/skyscrapers/terraform-instances//puppet-userdata?ref=2.3.2"
-  customer            = "${var.project}"
-  environment         = "${var.environment}"
-  function            = "${var.proxyname}"
+  source              = "github.com/skyscrapers/terraform-instances//puppet-userdata?ref=3.0.1"
+  customer            = var.project
+  environment         = var.environment
+  function            = var.proxyname
   amount_of_instances = 2
 }
 
 resource "aws_efs_file_system" "hacaddy_efs" {
   creation_token = "efs-${var.proxyname}-${var.project}-${var.environment}"
 
-  tags {
+  tags = {
     Name        = "efs-${var.proxyname}-${var.project}-${var.environment}"
-    Environment = "${var.environment}"
-    Project     = "${var.project}"
+    Environment = var.environment
+    Project     = var.project
   }
 }
 
 resource "aws_efs_mount_target" "hacaddy_efs_target" {
-  count           = "${var.subnet_count}"
-  file_system_id  = "${aws_efs_file_system.hacaddy_efs.id}"
-  subnet_id       = "${var.subnet_ids[count.index]}"
-  security_groups = ["${aws_security_group.sg_hacaddy.id}"]
+  count           = var.subnet_count
+  file_system_id  = aws_efs_file_system.hacaddy_efs.id
+  subnet_id       = var.subnet_ids[count.index]
+  security_groups = [aws_security_group.sg_hacaddy.id]
 }
 
 resource "aws_eip" "hacaddy_eip" {
   count    = 2
-  instance = "${module.hacaddy_instance.instance_ids[count.index]}"
+  instance = module.hacaddy_instance.instance_ids[count.index]
   vpc      = true
 
   lifecycle {
-    ignore_changes = ["instance"]
+    ignore_changes = [instance]
   }
 }
 
 resource "aws_iam_role_policy" "hacaddy_policy" {
   name = "policy_${var.proxyname}_${var.project}_${var.environment}"
-  role = "${module.hacaddy_instance.role_id}"
+  role = module.hacaddy_instance.role_id
 
   policy = <<EOF
 {
@@ -71,101 +71,103 @@ resource "aws_iam_role_policy" "hacaddy_policy" {
   ]
 }
 EOF
+
 }
 
 resource "aws_security_group" "sg_hacaddy" {
-  name        = "sg_${var.proxyname}_${var.project}_${var.environment}"
+  name = "sg_${var.proxyname}_${var.project}_${var.environment}"
   description = "Security group for hacaddy"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id = var.vpc_id
 
-  tags {
-    Name        = "${var.project}-${var.environment}-sg_${var.proxyname}"
-    Environment = "${var.environment}"
-    Project     = "${var.project}"
+  tags = {
+    Name = "${var.project}-${var.environment}-sg_${var.proxyname}"
+    Environment = var.environment
+    Project = var.project
   }
 }
 
 ## INGRESS
 
 resource "aws_security_group_rule" "hacaddy_http_ingress" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.sg_hacaddy.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "hacaddy_https_ingress" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.sg_hacaddy.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "hacaddy_icmp_ingress" {
-  type                     = "ingress"
-  from_port                = "-1"
-  to_port                  = "-1"
-  protocol                 = "icmp"
-  security_group_id        = "${aws_security_group.sg_hacaddy.id}"
-  source_security_group_id = "${aws_security_group.sg_hacaddy.id}"
+  type = "ingress"
+  from_port = "-1"
+  to_port = "-1"
+  protocol = "icmp"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  source_security_group_id = aws_security_group.sg_hacaddy.id
 }
 
 resource "aws_security_group_rule" "hacaddy_vrrp_ingress" {
-  type                     = "ingress"
-  from_port                = "-1"
-  to_port                  = "-1"
-  protocol                 = "112"
-  security_group_id        = "${aws_security_group.sg_hacaddy.id}"
-  source_security_group_id = "${aws_security_group.sg_hacaddy.id}"
+  type = "ingress"
+  from_port = "-1"
+  to_port = "-1"
+  protocol = "112"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  source_security_group_id = aws_security_group.sg_hacaddy.id
 }
 
 resource "aws_security_group_rule" "hacaddy_efs_ingress" {
-  type                     = "ingress"
-  from_port                = 2049
-  to_port                  = 2049
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.sg_hacaddy.id}"
-  source_security_group_id = "${aws_security_group.sg_hacaddy.id}"
+  type = "ingress"
+  from_port = 2049
+  to_port = 2049
+  protocol = "tcp"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  source_security_group_id = aws_security_group.sg_hacaddy.id
 }
 
 ## EGRESS
 
 resource "aws_security_group_rule" "hacaddy_icmp_egress" {
-  type                     = "egress"
-  from_port                = "-1"
-  to_port                  = "-1"
-  protocol                 = "icmp"
-  security_group_id        = "${aws_security_group.sg_hacaddy.id}"
-  source_security_group_id = "${aws_security_group.sg_hacaddy.id}"
+  type = "egress"
+  from_port = "-1"
+  to_port = "-1"
+  protocol = "icmp"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  source_security_group_id = aws_security_group.sg_hacaddy.id
 }
 
 resource "aws_security_group_rule" "hacaddy_puppet_egress" {
-  type              = "egress"
-  from_port         = 8140
-  to_port           = 8140
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.sg_hacaddy.id}"
-  cidr_blocks       = ["176.58.117.244/32"]
+  type = "egress"
+  from_port = 8140
+  to_port = 8140
+  protocol = "tcp"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  cidr_blocks = ["176.58.117.244/32"]
 }
 
 resource "aws_security_group_rule" "hacaddy_vrrp_egress" {
-  type                     = "egress"
-  from_port                = "-1"
-  to_port                  = "-1"
-  protocol                 = "112"
-  security_group_id        = "${aws_security_group.sg_hacaddy.id}"
-  source_security_group_id = "${aws_security_group.sg_hacaddy.id}"
+  type = "egress"
+  from_port = "-1"
+  to_port = "-1"
+  protocol = "112"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  source_security_group_id = aws_security_group.sg_hacaddy.id
 }
 
 resource "aws_security_group_rule" "hacaddy_efs_egress" {
-  type                     = "egress"
-  from_port                = 2049
-  to_port                  = 2049
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.sg_hacaddy.id}"
-  source_security_group_id = "${aws_security_group.sg_hacaddy.id}"
+  type = "egress"
+  from_port = 2049
+  to_port = 2049
+  protocol = "tcp"
+  security_group_id = aws_security_group.sg_hacaddy.id
+  source_security_group_id = aws_security_group.sg_hacaddy.id
 }
+
